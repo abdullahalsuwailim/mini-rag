9d90a101f45709db933a0a258db1280f79bd368f
blooz <blooz7386@gmail.com>
2026-07-05 20:36:38 +0300

    update base

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, APIRouter, Depends
 import os
-from src.helpers.config import get_settings
+from src.helpers.config import get_settings, Settings
 base_router = APIRouter(    
  prefix = "/api/v1",
  tags = ["api_v1"]
@@ -9,7 +9,7 @@ base_router = APIRouter(
 
 @base_router.get("/")
 
-async def welcome(app_settings = Depends(get_settings)):
+async def welcome(app_settings: Settings = Depends(get_settings)):
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
     return {
