535f571f12651a3ab8f13dc977cb905bd205be18
blooz <blooz7386@gmail.com>
2026-07-09 08:46:31 +0300

    add docker,dbschemes,and more

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list 
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    
+    MONGODB_DATABASE: str
+    MONGODB_URL: str
 
     model_config = SettingsConfigDict(env_file=BASE_DIR / ".env")
 
