1f06843200c1520d2a2f668d3b57e125e4e05ec5
blooz <blooz7386@gmail.com>
2026-07-12 14:10:39 +0300

    Add processing with out file_id givin

--- src/models/enum/ResponseEnum.py
+++ src/models/enum/ResponseEnum.py
@@ -1,9 +1,11 @@
 from enum import Enum 
 
 class ResponseSignal(Enum):
-    FILE_TYPE_NOT_ALLOWED = "this file type is not allowed"
-    FILE_TOO_LARGE = "this file is too large"
+    FILE_TYPE_NOT_ALLOWED = "this_file_type_is_not_allowed"
+    FILE_TOO_LARGE = "this_file_is_too_large"
     FILE_UPLOAD_SUCCESS = "success"
-    FILE_UPLOAD_FAILED = "file upload failed"
+    FILE_UPLOAD_FAILED = "file_upload_failed"
     PROCESSING_SUCCESS = "success"
-    PROCESSING_FAILED = "processing failed"
+    PROCESSING_FAILED = "processing_failed"
+    NO_FILE_ERROR = "no_file_found"
+    FILE_ID_ERROR = "no_file_found_with_this_id"
